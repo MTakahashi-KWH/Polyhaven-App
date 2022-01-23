bad55784769b78e9270eb1dd44abfaf6b939e004
Masashi Takahashi <mrt00021@mix.wvu.edu>
2022-01-23 15:30:12 -0500

    made navbar component and installed bootstrap

---
+++ src/app/navigation/Topnavbar.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-topnavbar',
+    templateUrl: 'Topnavbar.component.html',
+    styleUrls: ['Topnavbar.component.css']
+
+})
+
+export class TopNavBarComponent {
+
+
+}
