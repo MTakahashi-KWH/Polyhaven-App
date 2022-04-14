922abd8f484415f9929c02165d01b891b40db717
Masashi Takahashi <mrt00021@mix.wvu.edu>
2022-04-13 22:24:08 -0400

    set up login authentication

--- src/app/auth/authentication.service.ts
+++ src/app/auth/authentication.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { environment } from "src/environments/environment";
+import { AuthResponse } from "./authresponse";
 
 @Injectable({
     providedIn: 'root'
@@ -8,6 +9,7 @@ import { environment } from "src/environments/environment";
 export class AuthService{
     private baseUrl: string = 'https://identitytoolkit.googleapis.com/v1/accounts';
     private signUpEndpoint: string = 'signUp';
+    private logInEndpoint: string = 'signInWithPassword';
 
     public constructor(private http:HttpClient){
     }
@@ -17,6 +19,15 @@ export class AuthService{
             'password': password,
             'returnSecureToken': true
         }
-        return this.http.post(this.baseUrl + ':' + this.signUpEndpoint + '?key='+ environment.firebase.apiKey,requestPayload);
+        return this.http.post<AuthResponse>(this.baseUrl + ':' + this.signUpEndpoint + '?key='+ environment.firebase.apiKey,requestPayload);
+    }
+    public login(email: string, password: string){
+        const requestPayload = {
+            'email': email,
+            'password': password,
+            'returnSecureToken': true
+        }
+        return this.http.post<AuthResponse>(this.baseUrl + ':' + this.logInEndpoint + '?key='+ environment.firebase.apiKey,requestPayload);
+
     }
 }
--- src/app/auth/authentication/authentication.component.ts
+++ src/app/auth/authentication/authentication.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Observable } from 'rxjs';
 import { AuthService } from '../authentication.service';
+import { AuthResponse } from '../authresponse';
 
 @Component({
   selector: 'app-authentication',
@@ -8,6 +10,8 @@ import { AuthService } from '../authentication.service';
   styleUrls: ['./authentication.component.css']
 })
 export class AuthenticationComponent implements OnInit {
+  buttontype: string = "login";
+  private loginAware!: Observable<AuthResponse>;
 
   constructor(private auth:AuthService) { }
 
@@ -17,9 +21,26 @@ export class AuthenticationComponent implements OnInit {
   onSubmit(data: NgForm){
     console.log("login clicked");
     console.log(data);
-    this.auth.signup( 
-      data.value.email,
-      data.value.password
-    ).subscribe(data =>{console.log(data);});
+    if(this.buttontype=="signup"){
+      this.loginAware = this.auth.signup( 
+        data.value.email,
+        data.value.password
+      );
+    }else{
+      this.loginAware = this.auth.login( 
+        data.value.email,
+        data.value.password
+      );
+
+    }
+    this.loginAware.subscribe(
+      (data:AuthResponse) =>{
+        console.log(data);
+      },
+      error =>{
+        console.error(error.error)
+      }
+    )
+    console.log("you are logged in");
   }
 }
